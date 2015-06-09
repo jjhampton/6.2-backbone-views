f6c60306ac757c5d6f48d2f9ad62fc8737ed28ef
John Hampton <john_j_hampton@yahoo.com>
2015-06-09 17:26:01 -0400

    Create LikeModel class w/ default count attribute and like function to increment count

--- scripts/main.js
+++ scripts/main.js
@@ -2,5 +2,18 @@
   'use strict';
 
   $(document).ready(function(){
+    var LikeModel = Backbone.Model.extend({
+      defaults: {
+        count: 0
+      },
+      like: function() {
+        var currentCount = this.get(count);
+        console.log("Current count is" + currentCount);
+        this.set({count: currentCount + 1});
+        console.log("Set count is" + this.set);
+      }
+    });
+
   });
+
 })();
