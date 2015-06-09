99d7060e18128abc180c1d35e22b25a12cfdef87
John Hampton <john_j_hampton@yahoo.com>
2015-06-09 18:37:42 -0400

    Add instances of LikeModel and PageView to page Alert to screen when Like button is clicked

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,41 @@
 (function(){
   'use strict';
 
-  $(document).ready(function(){
+    console.log("TEST");
+
     var LikeModel = Backbone.Model.extend({
       defaults: {
         count: 0
       },
+
       like: function() {
-        var currentCount = this.get(count);
+        var currentCount = this.get('count');
         console.log("Current count is" + currentCount);
-        this.set({count: currentCount + 1});
-        console.log("Set count is" + this.set);
+        this.set({'count': currentCount + 1});
+        console.log("Set count is" + this.get('count'));
       }
     });
 
-  });
+    var PageView = Backbone.View.extend({
+      template: JST.likeButton,
 
+      events: {
+        'click': 'alertTest',
+      },
+      initialize: function() {
+        this.render();
+      },
+      render: function() {
+        var count = this.model.get('count');
+        console.log("Count is " + count);
+        this.$el.html(this.template(count));
+        $('.container').html(this.el);
+      },
+      alertTest: function() {
+        alert("Click pressed");
+      }
+    });
+
+    var currentLikes = new LikeModel();
+    var currentPage = new PageView({model: currentLikes});
 })();
