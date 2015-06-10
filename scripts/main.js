c9b1fdabaef8693db1b320ba44c30435704a4d69
John Hampton <john_j_hampton@yahoo.com>
2015-06-10 00:22:13 -0400

    Use main.js for submit button view & model, rename original main.js to mainLikeButton.js while developing submit button Create and modify submit button Handlebars template to disable button after click Refactor original main.js to work for submit button view & model with desired behavior

--- scripts/main.js
+++ scripts/main.js
@@ -1,49 +1,59 @@
 (function(){
   'use strict';
 
-    var LikeModel = Backbone.Model.extend({
+    var LoadingModel = Backbone.Model.extend({
       defaults: {
-        count: 0,
-        buttonText: "Likes" //starting value when at 0
+        //starting values before it is clicked:
+        isLoading: false,
+        text: "Submit"
       },
 
-      like: function() {
-        var buttonText;
-        var initialCount = this.get('count');
-        this.set({'count': initialCount + 1});
-        buttonText = this.get('count') > 1 ? 'Likes' : 'Like';
-        this.set({'buttonText': buttonText});
-        }
+      // load: function() {
+      //   var text;
+      //   // var initialCount = this.get('count');
+      //   // this.set({'count': initialCount + 1});
+      //   text = this.get('count') > 1 ? 'Likes' : 'Like';
+      //   this.set({'text': text});
+      //   }
     });
 
     var PageView = Backbone.View.extend({
-      tagName: 'button',
-      template: JST.likeButton,
+      template: JST.submitButtonTemplate,
 
       events: {
         'click': function() {
-          this.model.like();
+          console.log("clicked");
+          var that = this;
+          this.model.set({'text': 'Loading...'});
+          this.model.set({'isLoading': true});
+          //change text CSS to lighter gray
           this.render();
+          setTimeout(function() {
+            that.model.set({'text': 'Submit'});
+            that.model.set({'isLoading': false});
+            that.render();
+          }, 4000);
         }
       },
       initialize: function() {
         this.render();
       },
       render: function() {
-        var count = this.model.get('count');
-        var buttonText = this.model.get('buttonText');
+        var text = this.model.get('text');
+        var isLoading = this.model.get('isLoading');
+
         this.$el.html(this.template({
-          count: count,
-          buttonText: buttonText
+          text: text,
+          isLoading: isLoading,
           }));
         return this;
       },
     });
 
 
-    var currentLikes = new LikeModel();
-    var currentPage = new PageView({model: currentLikes});
-    $('.container').html(currentPage.$el);
+    var submitButtonModel = new LoadingModel();
+    var submitButtonView = new PageView({model: submitButtonModel});
+    $('.container').html(submitButtonView.$el);
 
 
 })();
---
+++ scripts/mainLikeButton.js
@@ -0,0 +1,48 @@
+(function(){
+  'use strict';
+
+    var LikeModel = Backbone.Model.extend({
+      defaults: {
+        count: 0,
+        buttonText: "Likes" //starting value when at 0
+      },
+
+      like: function() {
+        var buttonText;
+        var initialCount = this.get('count');
+        this.set({'count': initialCount + 1});
+        buttonText = this.get('count') > 1 ? 'Likes' : 'Like';
+        this.set({'buttonText': buttonText});
+        }
+    });
+
+    var PageView = Backbone.View.extend({
+      template: JST.likeButtonTemplate,
+
+      events: {
+        'click': function() {
+          this.model.like();
+          this.render();
+        }
+      },
+      initialize: function() {
+        this.render();
+      },
+      render: function() {
+        var count = this.model.get('count');
+        var buttonText = this.model.get('buttonText');
+        this.$el.html(this.template({
+          count: count,
+          buttonText: buttonText
+          }));
+        return this;
+      },
+    });
+
+
+    var currentLikes = new LikeModel();
+    var currentPage = new PageView({model: currentLikes});
+    $('.container').html(currentPage.$el);
+
+
+})();
